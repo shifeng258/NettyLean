19480d3b7dbaa938b77837d107630c6135894d32
shifeng258 <shifeng2400@126.com>
2015-08-19 21:02:14 +0800

    init

---
+++ src/io/netty/example/time/TimeServerHandler.java
@@ -0,0 +1,40 @@
+package io.netty.example.time;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelFutureListener;
+import io.netty.channel.ChannelHandlerAdapter;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.handler.logging.LoggingHandler;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.Date;
+
+public class TimeServerHandler extends ChannelHandlerAdapter {
+
+    @Override
+    public void channelActive(final ChannelHandlerContext ctx) throws UnknownHostException { // (1)
+        System.out.println("Host has sent the server time");
+        ctx.writeAndFlush("Welcome to " + InetAddress.getLocalHost().getHostName() + "!\r\n");
+        ctx.writeAndFlush("It is " + new Date() + " now.\r\n");
+        final ByteBuf time = ctx.alloc().buffer(4); // (2)
+        time.writeInt((int) (System.currentTimeMillis() / 1000L + 2208988800L));
+
+        final ChannelFuture f = ctx.writeAndFlush(time); // (3)
+
+        f.addListener(new ChannelFutureListener() {
+
+            public void operationComplete(ChannelFuture future) {
+                assert f == future;
+                ctx.close();
+            }
+        }); // (4)
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
+        cause.printStackTrace();
+        ctx.close();
+    }
+}
